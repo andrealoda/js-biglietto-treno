a902213edbba980a69f135a53cc3661581e1aa78
andrealoda <andrea@casaloda.it>
2026-02-27 20:03:16 +0100

    Add instructions and pseudo-code in main.js

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+// JS-BIGLIETTO-TRENO
+
+/*
+📋 Il programma deve chiedere all'utente il numero di km da percorrere e l'età del passeggero.
+
+Il biglietto costa 0.21€/km, ma si applicano i seguenti sconti:
+- sconto 20% per i minorenni
+- sconto 40% per gli over 65 (anche se in Italia i pensionati sono quelli con la capacità di spesa più alta...)
+
+Gli output devono avere 2 cifre decimali.
+
+Possiamo testare la logica con i seguenti esempi:
+- 100km / 10 anni / 16.80€
+- 100km / 70 anni / 12.60€
+*/
+
+// ➡️ Definiamo le variabili che ci serviranno per la logica
+
+/*
+🧮 Definiamo la logica di calcolo del prezzo del biglietto considerando che:
+- lo sconto del 20% per i minorenni equivale all'80% del prezzo del biglietto intero
+- lo sconto del 40% per gli over65 equivale al 60% del prezzo del biglietto intero.
+*/
