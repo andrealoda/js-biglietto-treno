e5c4e6fbaec6f308d5251b7f43f15e8558b02b24
andrealoda <andrea@casaloda.it>
2026-02-28 09:54:43 +0100

    saturday morning task "età" implemented (substitute for js-biglietto-treno task)

--- assets/main.js
+++ assets/main.js
@@ -15,12 +15,12 @@ Possiamo testare la logica con i seguenti esempi:
 */
 
 //➡️ Definiamo le variabili che ci serviranno per la logica
-const welcomeMessage = alert('🧑‍✈️ Benvenuto in NSI, il NUOVO SISTEMA INTELLIGENTE di calcolo del prezzo del biglietto.');
-const discountMessage = alert('Ti ricordiamo che se hai meno di 18 o più di 65 anni avrai diritto a uno sconto!');
-const tripKm = Number(prompt('indica quanti kilometri vuoi percorrere'));
-const passengerAge = Number(prompt('indica la tua età'));
-const costKm = 0.21;
-const ticketCalc = tripKm * costKm;
+// const welcomeMessage = alert('🧑‍✈️ Benvenuto in NSI, il NUOVO SISTEMA INTELLIGENTE di calcolo del prezzo del biglietto.');
+// const discountMessage = alert('Ti ricordiamo che se hai meno di 18 o più di 65 anni avrai diritto a uno sconto!');
+// const tripKm = Number(prompt('indica quanti kilometri vuoi percorrere'));
+// const passengerAge = Number(prompt('indica la tua età'));
+// const costKm = 0.21;
+// const ticketCalc = tripKm * costKm;
 
 /*
 🧮 Definiamo la logica di calcolo del prezzo del biglietto considerando che:
@@ -30,16 +30,33 @@ const ticketCalc = tripKm * costKm;
 
 // MODELLO DI LOGICA
 
-if (passengerAge < 18) {
-    const ticketPrice = ticketCalc * 0.8;
-    document.getElementById('price').textContent = ticketPrice.toFixed(2);
-    console.log(tripKm, passengerAge, ticketPrice.toFixed(2)); //utilizziamo il console.log per verificare i valori immessi dall'utente
-} else if (passengerAge > 65) {
-    const ticketPrice = ticketCalc * 0.6;
-    document.getElementById('price').textContent = ticketPrice.toFixed(2);
-    console.log(tripKm, passengerAge, ticketPrice.toFixed(2)); //utilizziamo il console.log per verificare i valori immessi dall'utente
+// if (passengerAge < 18) {
+//     const ticketPrice = ticketCalc * 0.8;
+//     document.getElementById('price').textContent = ticketPrice.toFixed(2);
+//     console.log(tripKm, passengerAge, ticketPrice.toFixed(2)); //utilizziamo il console.log per verificare i valori immessi dall'utente
+// } else if (passengerAge > 65) {
+//     const ticketPrice = ticketCalc * 0.6;
+//     document.getElementById('price').textContent = ticketPrice.toFixed(2);
+//     console.log(tripKm, passengerAge, ticketPrice.toFixed(2)); //utilizziamo il console.log per verificare i valori immessi dall'utente
+// } else {
+//     const ticketPrice = ticketCalc;
+//     document.getElementById('price').textContent = ticketPrice.toFixed(2);
+//     console.log(tripKm, passengerAge, ticketPrice.toFixed(2)); //utilizziamo il console.log per verificare i valori immessi dall'utente
+// }
+
+
+///////////////////////////////////////////////////////////
+
+// chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+
+const startMessage = alert('Ciao amici! Ditemi la vostra età ed io farò una magia 🧙‍♂️');
+const firstPersonAge = Number(prompt('Il primo di voi scriva qui quanti anni ha', 'es. 30'));
+const secondPersonAge = Number(prompt('Il secondo di voi inserisca qui quanti anni ha', 'es. 20'));
+
+if (firstPersonAge > secondPersonAge) {
+    const caseOne = alert('🪄 Il primo di voi che ha inserito la sua età è piu vecchio');
+} else if (secondPersonAge > firstPersonAge) {
+    const caseTwo = alert('🪄 Il secondo di voi che ha inserito la sua età è piu vecchio');
 } else {
-    const ticketPrice = ticketCalc;
-    document.getElementById('price').textContent = ticketPrice.toFixed(2);
-    console.log(tripKm, passengerAge, ticketPrice.toFixed(2)); //utilizziamo il console.log per verificare i valori immessi dall'utente
+    const caseThree = alert('😱 Siete coetanei! Che bello! Festeggiamo... 🍻 Birretta?')
 }
